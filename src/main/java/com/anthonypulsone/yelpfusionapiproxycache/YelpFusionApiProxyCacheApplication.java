c8ef79a6827f6aba012acedef2d4e342ec6618cd
anthonypulsone <anthonyjpulsone@gmail.com>
2022-11-26 19:30:26 -0500

    initial commit

---
+++ src/main/java/com/anthonypulsone/yelpfusionapiproxycache/YelpFusionApiProxyCacheApplication.java
@@ -0,0 +1,13 @@
+package com.anthonypulsone.yelpfusionapiproxycache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpFusionApiProxyCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpFusionApiProxyCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anthonypulsone/yelpfusionapiproxycache/YelpFusionApiProxyCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonypulsone.yelpfusionapiproxycache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelpFusionApiProxyCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
